57302757ae0dd961bfe5f06b26fe355e592349ba
Lyn Ho <lyn4develop@gmail.com>
2019-10-16 11:06:54 +0800

    0011.container-with-most-water

---
+++ 11.盛最多水的容器.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=javascript
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+  if (!height || height.length <= 1) return 0
+
+  let left = 0
+  let right = height.length - 1
+
+  let max = 0
+
+  while (left < right) {
+    const currentArea = (right - left) * Math.min(height[left], height[right])
+
+    if (currentArea > max) {
+      max = currentArea
+    }
+
+    if (height[left] < height[right]) {
+      left++
+    } else {
+      right--
+    }
+  }
+
+  return max
+}
+// @lc code=end
