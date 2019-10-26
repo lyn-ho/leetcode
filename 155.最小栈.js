21cffb4442b7e475df1601bd3530f18472fc4d8c
Lyn Ho <lyn4develop@gmail.com>
2019-10-26 21:17:43 +0800

    155.min-stack

---
+++ 155.最小栈.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=155 lang=javascript
+ *
+ * [155] 最小栈
+ */
+
+// @lc code=start
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+  this.stack = []
+  this.min = Number.MAX_VALUE
+}
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+  const min = this.min
+
+  if (x < this.min) {
+    this.min = x
+  }
+
+  return this.stack.push(x - min)
+}
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+  const item = this.stack.pop()
+  const min = this.min
+
+  if (item < 0) {
+    this.min = min - item
+    return min
+  }
+  return item + min
+}
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+  const item = this.stack[this.stack.length - 1]
+  const min = this.min
+
+  if (item < 0) {
+    return min
+  }
+
+  return item + min
+}
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+  return this.min
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+// @lc code=end
