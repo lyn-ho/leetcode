a358c1b132123585e708908d84af81a424d26f4b
Lyn Ho <lyn4develop@gmail.com>
2020-04-18 22:48:50 +0800

    9.回文数

---
+++ easy/9.回文数.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=javascript
+ *
+ * [9] 回文数
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  return x == (x || 0).toString().split('').reverse().join('')
+};
+// @lc code=end
+
