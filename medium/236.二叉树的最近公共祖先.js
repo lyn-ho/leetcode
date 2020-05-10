72bedcef57a536c3df903c9d9e8f0cbfe1d884d2
Lyn Ho <lyn4develop@gmail.com>
2020-05-10 13:08:24 +0800

    236.二叉树的最近公共祖先

---
+++ medium/236.二叉树的最近公共祖先.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=javascript
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+  if (!root || root === p || root === q) return root
+
+  const left = lowestCommonAncestor(root.left, p, q)
+  const right = lowestCommonAncestor(root.right, p, q)
+
+  if (!left) return right // 左子树找不到，返回右子树
+  if (!right) return left // 右子树找不到，返回左子树
+
+  return root // 左右子树分别有一个，返回 root
+};
+// @lc code=end
+
