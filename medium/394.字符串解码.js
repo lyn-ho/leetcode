5a7d025895245d15d83da992c73384ffd2f42326
Lyn Ho <lyn4develop@gmail.com>
2020-06-04 09:46:39 +0800

    update: 394.字符串解码

--- medium/394.字符串解码.js
+++ medium/394.字符串解码.js
@@ -11,6 +11,20 @@
  */
 var decodeString = function(s) {
   // 栈
+  // str, strStack, multi, multiStack
+  // 遍历字符串 s ，获取字符 c
+  //  当 c in ['0'-'9'], 拼接到重复数量 multi ，用于倍数计算
+  //  当 c in ['a'-'z','A'-'Z']，拼接到 str
+  //  当 c === '[' 将 multi 和 str 入栈，并置空
+  //   str 入栈，用于发现对应 ']' 后的拼接操作
+  //   multi 入栈，用于发现对应 ']' 后，multi * [...]
+  //  单 c === ']' , strStack 出栈，拼接字符串 res = lastStr + curMulti * str
+  //   lastStr 上一个 '[' 到当前 '[' 的字符串
+  //   curMulti 当前 '[' 到 ']' 字符串重复的倍数 multiStack.pop()
+  //  返回 str
+
+  // 时间复杂度 O(n)
+
   let str = ''
   // 字符串栈
   let strStack = []
