26b0fd178788d4a97da9cdaded46279447f3d8a5
Lyn Ho <lyn4develop@gmail.com>
2020-06-07 13:18:22 +0800

    Update: 206.反转链表

--- easy/206.反转链表.js
+++ easy/206.反转链表.js
@@ -16,7 +16,11 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) {
+var reverseList = function (head) {
+  // 定义两个指针： pre 和 cur ，pre 在前， cur 在后
+  // 每次让 pre 的 next 指向 cur ，实现一次局部反转
+  // 局部反转后，pre 和 cur 同时往前移动一位
+  // 循环上述过程，直到 pre 到达链尾
   if (!head || !head.next) return head
 
   let cur = head
