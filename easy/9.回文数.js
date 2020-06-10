5fefdf27f55a1bd9e9c4f1951cd081e5db36652a
Lyn Ho <lyn4develop@gmail.com>
2020-06-10 23:40:34 +0800

    update: 9.回文数

--- easy/9.回文数.js
+++ easy/9.回文数.js
@@ -10,7 +10,20 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-  return x == (x || 0).toString().split('').reverse().join('')
+  if (!x) return true
+
+  let str = x.toString()
+  let l = 0
+  let r = str.length - 1
+
+  while (l < r) {
+    if(str[l++] !== str[r--]) return false
+  }
+
+  return true
 };
+// var isPalindrome = function(x) {
+//   return x == (x || 0).toString().split('').reverse().join('')
+// };
 // @lc code=end
 
