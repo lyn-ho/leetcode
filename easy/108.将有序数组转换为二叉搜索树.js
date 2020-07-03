1f43ae0f4a381744965f4ea425585f2af3505760
Lyn Ho <lyn4develop@gmail.com>
2020-07-03 09:08:14 +0800

    108.将有序数组转换为二叉搜索树

---
+++ easy/108.将有序数组转换为二叉搜索树.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=javascript
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+// var sortedArrayToBST = function(nums) {
+//   return buildBST(nums, 0, nums.length - 1)
+// };
+
+// function buildBST(nums, start, end) {
+//   if (start > end) return null
+
+//   const mid = (start + end) >>> 1
+//   const root = new TreeNode(nums[mid])
+
+//   root.left = buildBST(nums, start, mid - 1)
+//   root.right = buildBST(nums, mid + 1, end)
+
+//   return root
+// }
+// @lc code=end
+
---
+++ easy/108.将有序数组转换为二叉搜索树.ts
@@ -0,0 +1,29 @@
+
+// Definition for a binary tree node.
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+
+function sortedArrayToBST(nums: number[]): TreeNode | null {
+  return createBST(nums, 0, nums.length - 1)
+};
+
+function createBST(nums: number[], start: number, end: number): TreeNode | null {
+  if (start < end) return null
+
+  const mid = (start + end) >>> 1
+  const root: TreeNode = new TreeNode(nums[mid])
+
+  root.left = createBST(nums, start, mid - 1)
+  root.right = createBST(nums, mid + 1, end)
+
+  return root
+}
