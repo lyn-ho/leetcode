47db74808f4b1709fff6c79f2f45da5e6962d6d9
Lyn Ho <lyn4develop@gmail.com>
2020-07-06 22:54:59 +0800

    62.不同路径 ts

---
+++ medium/62.不同路径.ts
@@ -0,0 +1,11 @@
+function uniquePaths(m: number, n: number): number {
+  const dp: number[] = Array(n).fill(1)
+
+  for (let i = 1;i < m;i++) {
+    for (let j = 1;j < n;j++) {
+      dp[j] = dp[j] + dp[j - 1]
+    }
+  }
+
+  return dp[n - 1]
+};
