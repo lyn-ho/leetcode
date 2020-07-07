1063d3f9821082b5d1bdec0e2c2b58d1d334cc72
Lyn Ho <lyn4develop@gmail.com>
2020-07-07 23:42:09 +0800

    206.反转链表 ts

---
+++ easy/206.反转链表.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let cur = head
+  let pre = null
+
+  while (cur) {
+    const next = cur.next
+    cur.next = pre
+    pre = cur
+    cur = next
+  }
+
+  return pre
+};
