14f8d3add3a40eab48ee92e5424de18f8ab7112e
Lyn Ho <lyn4develop@gmail.com>
2020-07-07 23:31:25 +0800

    112.路径总和

---
+++ easy/112.路径总和.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=javascript
+ *
+ * [112] 路径总和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, sum) {
+  if (!root) return false
+
+  if (!root.left && !root.right) return root.val === sum
+
+  return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val)
+};
+// @lc code=end
+
